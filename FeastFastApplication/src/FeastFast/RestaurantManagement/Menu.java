18871324acc809c09372a8b04da73bd077c89555
B-i-x <bix.romero@gmail.com>
2023-11-08 16:24:41 -0700

    added restaurant cases

--- FeastFastApplication/src/FeastFast/RestaurantManagement/Menu.java
+++ FeastFastApplication/src/FeastFast/RestaurantManagement/Menu.java
@@ -15,8 +15,41 @@ public class Menu {
         menuItems.add(item);
     }
 
+    public void removeItem(MenuItem item) {
+        menuItems.remove(item);
+    }
+
+    public void updateItem(MenuItem updatedItem) {
+        // Find the item in the list with the same ID and replace it with the updated item
+        for (int i = 0; i < menuItems.size(); i++) {
+            MenuItem currentItem = menuItems.get(i);
+            if (currentItem.getId().equals(updatedItem.getId())) {
+                menuItems.set(i, updatedItem);
+                break;
+            }
+        }
+    }
+
     public boolean containsItem(MenuItem item) {
         return menuItems.contains(item);
+    }
+
+    public boolean containsItem(String itemName) {
+        for (MenuItem item : menuItems) {
+            if (item.getName().equalsIgnoreCase(itemName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public MenuItem getItem(String itemName) {
+        for (MenuItem item : menuItems) {
+            if (item.getName().equalsIgnoreCase(itemName)) {
+                return item;
+            }
+        }
+        return menuItems.get(0);
     }
 
     public List<MenuItem> getItems() {
