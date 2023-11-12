84931eb007867b80a16028d7a2deb130033b0f72
B-i-x <bix.romero@gmail.com>
2023-11-11 19:41:22 -0700

    reworking

--- src/FeastFast/RestaurantManagement/Order.java
+++ src/FeastFast/RestaurantManagement/Order.java
@@ -5,9 +5,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BooleanSupplier;
 
+import FeastFast.OrderingAndTransactions.CostCalculator;
 import FeastFast.UserManagement.Customer;
 import FeastFast.UserManagement.Restaurant;
 import FeastFast.UserManagement.DeliveryDriver;
+
 public class Order {
 
     public static enum Status {
@@ -30,6 +32,8 @@ public class Order {
     private String comments;
 
     private ArrayList<MenuItem> foodItems = new ArrayList<>();
+    
+    private CostCalculator costCalculator;
 
     public Order() {
         // Initialize default values
@@ -37,15 +41,70 @@ public class Order {
         
     }
 
+    // Getter and setter for customer
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    // Getter and setter for deliveryDriver
+    public DeliveryDriver getDeliveryDriver() {
+        return deliveryDriver;
+    }
+
+    public void setDeliveryDriver(DeliveryDriver deliveryDriver) {
+        this.deliveryDriver = deliveryDriver;
+    }
+
+    // Getter and setter for restaurant
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    // Getter and setter for status
     public Status getStatus() {
         return status;
     }
 
-    public void setStatus(Status s) {
-        this.status = s;
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    // Getter and setter for comments
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    // Getter and setter for foodItems
+    public ArrayList<MenuItem> getFoodItems() {
+        return new ArrayList<>(foodItems); // Return a copy for encapsulation
+    }
+
+    public void setFoodItems(ArrayList<MenuItem> foodItems) {
+        this.foodItems = new ArrayList<>(foodItems); // Create a new list for encapsulation
+    }
+
+    // Getter and setter for costCalculator
+    public CostCalculator getCostCalculator() {
+        return costCalculator;
+    }
+
+    public void setCostCalculator(CostCalculator costCalculator) {
+        this.costCalculator = costCalculator;
     }
 
     
-    }
+    
 
 }
