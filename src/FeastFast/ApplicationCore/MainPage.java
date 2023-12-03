33674e5231730f3d78c3f8ea5646b395e5f122c3
Vaidehi Pujary <vaipujary@gmail.com>
2023-12-03 00:05:40 -0700

    some changes

--- src/FeastFast/ApplicationCore/MainPage.java
+++ src/FeastFast/ApplicationCore/MainPage.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.*;
 
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.Color;
@@ -20,6 +21,13 @@ public class MainPage extends JFrame {
 	JTextField customerNameText;
 	JTextField emailText;
 	JPasswordField passwordText;
+	
+	// Exit Icon
+	ImageIcon icon;
+	Image scaledIcon;
+	ImageIcon newScaledIcon;
+	JButton item;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -57,6 +65,11 @@ public class MainPage extends JFrame {
 		lblNewLabel_1.setFont(new Font("Bangla MN", Font.BOLD | Font.ITALIC, 41));
 		lblNewLabel_1.setBounds(154, 6, 533, 62);
 		getContentPane().add(lblNewLabel_1);
+		
+		// Person Icon
+		icon = new ImageIcon(Checkout.class.getResource("/FeastFast/ApplicationCore/ExitIcon.png"));
+		scaledIcon = icon.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT);
+		newScaledIcon = new ImageIcon(scaledIcon);
 
 		JLabel lblNewLabel = new JLabel(new ImageIcon(MainPage.class.getResource("/FeastFast/ApplicationCore/FeastFastGIF.gif")));
 		lblNewLabel.setBounds(154, 23, 500, 500);
@@ -64,27 +77,31 @@ public class MainPage extends JFrame {
 		
 		// Log In Button
 		btnNewButton = new JButton("Log In");
+		btnNewButton.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
 		btnNewButton.setOpaque(true);
 		btnNewButton.setBorderPainted(false);
-		btnNewButton.setBackground(new Color(234, 92, 100));
+		btnNewButton.setBackground(new Color(250, 128, 114));
 		btnNewButton.setBounds(77, 576, 168, 55);
 		getContentPane().add(btnNewButton);
 		btnNewButton.addActionListener(new Listener());
 		
 		// Sign Up Button
 		btnSignUp = new JButton("Sign Up");
+		btnSignUp.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
 		btnSignUp.setOpaque(true);
 		btnSignUp.setBorderPainted(false);
-		btnSignUp.setBackground(new Color(234, 92, 100));
+		btnSignUp.setBackground(new Color(250, 128, 114));
 		btnSignUp.setBounds(310, 576, 168, 55);
 		getContentPane().add(btnSignUp);
 		btnSignUp.addActionListener(new Listener());
 		
 		// Exit Button
-		btnExit = new JButton("Exit");
+		btnExit = new JButton(newScaledIcon);
+		btnExit.setText("Exit");
+		btnExit.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
 		btnExit.setOpaque(true);
 		btnExit.setBorderPainted(false);
-		btnExit.setBackground(new Color(234, 92, 100));
+		btnExit.setBackground(new Color(250, 128, 114));
 		btnExit.setBounds(572, 576, 168, 55);
 		btnExit.addActionListener(new Listener());
 		
